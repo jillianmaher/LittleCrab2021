8e06b742c635771317e6c023789a516f38d88168
Jillian Maher <jillian.maher2@boone.kyschools.us>
2021-03-15 08:36:56 -0400

    Finish crab actions, add Worm

--- Crab.java
+++ Crab.java
@@ -11,7 +11,18 @@ public class Crab extends Actor
         {
             turn(17);
         }
+        
+        if(Greenfoot.getRandomNumber(100) < 10 )
+        {
+            turn( Greenfoot.getRandomNumber(91) - 45 );
+        }
+        
         move();
+        
+        if( isTouching(Worm.class))
+        {
+            removeTouching( Worm.class );
+        }
     }
     
     public void move( )
